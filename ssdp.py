64aceaa04b70e833b08eb3b386a7ceccd4e5ec79
stkyle <stkyle@users.noreply.github.com>
2016-12-19 12:42:51 -0500

    Create ssdp.py

---
+++ ssdp.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/python
+"""
+"""
+
+import sys
+import re
+import socket
+import time
+import struct
+import logging
+
+log = logging.getLogger(__name__)
+
+class Header(object)
+    """
+    """
+    
